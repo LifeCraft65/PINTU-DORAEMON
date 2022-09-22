50e1f2397038b19db786508f60f14d3a585e6990
LifeCraft65 <114159613+LifeCraft65@users.noreply.github.com>
2022-09-22 19:56:05 +0700

    Upload Lif
    UPL

---
+++ life.js
@@ -0,0 +1,24 @@
+const alert1 = "Welcome To Lifecraft!";
+alert(alert1);
+
+const consolel1 = "Selamat Di lifecraft Konsol";
+console.log(consolel1);
+
+
+function d1s(){
+    alert("Yeah.. Welcome To Lifecraft Website");
+}
+
+function bt1d(){
+    window.open("https://discord.gg/rwWFpaw4");
+}
+
+function btns1(){
+    alert("Are You Real To Support Lifecraft?");
+    document.getElementById("dd1").innerHTML = "Thank You For Support Lifecraft";   
+}
+
+function dd1(){
+    alert("What? You Click Me?");
+    document.getElementById("dd1").innerHTML = "https://discord.gg/rwWFpaw4";
+}
